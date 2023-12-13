b742ff83a511fbf4a3111c02b9de4014663aee10
udayprakashspt <129489345+udayprakashspt@users.noreply.github.com>
2023-12-13 21:08:52 +0530

    Update pro.java

--- pro.java
+++ pro.java
@@ -3,7 +3,7 @@ class pro
    public static void main(String[] args)
 	{
 	  System.out.println("uday kumar hp");
-	  System.out.println("fsd");
+	  System.out.println("full stack development");
 	 System.out.println("spt");
 	}
  }
